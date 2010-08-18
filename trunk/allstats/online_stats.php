de38cd5bf8971ef90d23cdd2046820ac6fc88db2
avon.dn.ua <avon.dn.ua@d961353e-f77b-8356-d004-fbd8e57d8439>
2010-08-18 13:03:23 +0000

    Update online stats script. This is not final release

--- trunk/allstats/online_stats.php
+++ trunk/allstats/online_stats.php
@@ -1,39 +1,45 @@
 <?php
 
 
-function get_info()
+require_once("config.php");
+
+function get_info($bot)
 {
-	//return "|gamesinfo|--|-ar 1                           ,123,11121314143132515253,,|--|-ar 2                           ,123,1131,,|--|";
 
    $socketD = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP); 
 
-
    if($socketD === FALSE) { 
-       echo 'socket_create failed: '.socket_strerror(socket_last_error())."\n";
        return "err";
    }
   
-   if(!socket_bind($socketD, "127.0.0.1", 6973)) { // todo ВЫНЕСТИ В КОНФИГ, настройки слушаешего адреса
+   if(!socket_bind($socketD,$bot['ip_in'], $bot['port_in'])) {
        socket_close($socketD);
-       echo 'socket_bind failed: '.socket_strerror(socket_last_error())."\n";
        return "err";
    }
   
-	$fp = fsockopen('udp://127.0.0.1', 6969);  // todo ВЫНЕСТИ В КОНФИГ настройки командного адреса
+	$fp = fsockopen('udp://'.$bot['ip_out'], $bot['port_out']);  // todo ВЫНЕСТИ В КОНФИГ настройки командного адреса
 
 	if($fp)
 	{
-  		 fputs($fp, "||127.0.0.1 sendgamesstatus");
-   		 fclose($fp);
+		fputs($fp,"||".$bot['ip_self']." connect ".$bot['password']);
+	//	socket_recvfrom($socketD, $buf, $bot['port_in'], 0, $clientIP, $clientPort);
+	//	echo $buf;
+		//if($buf && !strstr($buf, "connected"))
+		//{
+		//	fclose($fp);
+		//	return "err";
+		//}
+  		fputs($fp, "||".$bot['ip_self']." sendgamesstatus");
+   		fclose($fp);
 	}
 
-   socket_recvfrom($socketD, $buf, 65535, 0, $clientIP, $clientPort);
+   socket_recvfrom($socketD, $buf, $bot['port_in'], 0, $clientIP, $clientPort);
 
    if($buf === FALSE) { 
-       echo 'socket_read() returned false : '.socket_strerror(socket_last_error())."\n";
+    
        return "err";
    } elseif(strlen($buf) === 0) { 
-       echo 'socket_read() returned an empty string : '.socket_strerror(socket_last_error())."\n";
+
        return "err";
    }
 
@@ -45,6 +51,60 @@ function get_info()
   return $buf;
 
 }
+
+
+/* Получаем статы от бота. */
+function get_stats($bot)
+{
+	$games = Array();
+	$i = 10;
+	while($i){	
+		$games_arr = explode("|--|", get_info($bot));
+		if($games_arr[0] == "|gamesinfo") break;
+		$i--;
+	}
+	if(!$i) return False;
+	
+	foreach(array_slice($games_arr, 1) as $g=>$v)
+	{
+		if($v == "") continue;
+		$games[] = explode(",", $v);    //  $games[] = array_merge($buf, explode(",", substr($v, 33)));
+		//print_r($games);
+	}
+	return $games;
+}
+
+/* Формируем результат в HTML. */
+function print_stats($stats, $bot_name){
+	$i = 0;
+	$out = "<tr>";
+	foreach($stats as $g=>$v)
+	{
+		$i++;
+
+		if ($i%2 != 0) $out .= "</tr><tr>";
+
+
+		$out .= "<th><font color=white>";
+		$out .= "Game name: ".trim($v[0])." <br />";
+		$out .= "Game time: ".(floor((int)trim($v[2]) / 60))." min ".((int)trim($v[2]) % 60)." sec <br />";
+		$out .= "<img src='map_img.php?kt=".trim($v[1])."' /></font></th>";
+
+	}
+	$out .= "</tr>";
+
+	return $out;
+}
+
+
+/* Проверяем работает ли бот. */
+function isBotAlive($bot)
+{
+	return True;
+}
+
+/************************************ Start here ***************************************/
+//require_once('conf.php');
 
 $create_cache = 1;
 $read_cache = 0;
@@ -75,25 +135,14 @@ if (!$db)
 
 if(!$read_cache)
 {
-	$i = 10;
-	while($i){	
-		$games_arr = explode("|--|", get_info());
-		if($games_arr[0] == "|gamesinfo") break;
-		$i--;
-	}
-	if(!$i) die("");
-	$games = Array();
-	foreach(array_slice($games_arr, 1) as $g=>$v)
+	foreach($bots as $b=>$bot)
 	{
-		if($v == "") continue;
-		$games[] = explode(",", $v);    //  $games[] = array_merge($buf, explode(",", substr($v, 33)));
-		//print_r($games);
-	}
-	foreach($games as $g=>$v)
-	{
-		$out .= "Game name: ".trim($v[0])."<br />";
-		$out .= "Game time: ".trim($v[2])."<br />";
-		$out .= "<img src='map_img.php?kt=".trim($v[1])."' /><br /><br />";
+		if(isBotAlive($bot))
+		{
+			$stats = get_stats($bot);
+			if($stats)
+				$out .= print_stats($stats, $bot['name']);
+		}
 	}
 	/*
 	if(create_cache)
